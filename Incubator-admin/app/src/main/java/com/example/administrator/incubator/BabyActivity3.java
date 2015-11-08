917898b6e5511b362ec3ddf0add470d2b97ca766
SungYoungMin <sym6288@naver.com>
2015-11-08 23:09:25 +0900

    2015/11/08

---
+++ Incubator-admin/app/src/main/java/com/example/administrator/incubator/BabyActivity3.java
@@ -0,0 +1,52 @@
+package com.example.administrator.incubator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2015-11-08.
+ */
+public class BabyActivity3 extends Activity {
+    TextView Name, Sex, Ssn, Bloodtype, State, Parent, Temperature, Humidity, Heartbeat, Sound;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.baby3);
+
+        Person baby = (Person) getIntent().getExtras().getParcelable("key");
+
+        Name=(TextView)findViewById(R.id.textViewNameValue);
+        Name.setText(baby.babyName);
+
+        Sex=(TextView)findViewById(R.id.textViewSexValue);
+        Sex.setText(baby.sex);
+
+        Ssn=(TextView)findViewById(R.id.textViewSSNValue);
+        Ssn.setText(baby.ssn);
+
+        Bloodtype=(TextView)findViewById(R.id.textViewBloodTypeValue);
+        Bloodtype.setText(baby.bloodType);
+
+        State=(TextView)findViewById(R.id.textViewStateValue);
+        State.setText(baby.state);
+
+        Parent=(TextView)findViewById(R.id.textViewParentValue);
+        Parent.setText(baby.parentName);
+
+        Temperature=(TextView)findViewById(R.id.textViewTemperatureValue);
+        Temperature.setText(baby.temperature);
+
+        Humidity=(TextView)findViewById(R.id.textViewHumidityValue);
+        Humidity.setText(baby.humidity);
+
+        Heartbeat=(TextView)findViewById(R.id.textViewHeartBeatValue);
+        Heartbeat.setText(baby.heartbeat);
+
+        Sound=(TextView)findViewById(R.id.textViewSoundValue);
+        Sound.setText(baby.sound);
+
+
+    }
+}
+
--- Incubator-admin/app/src/main/java/com/example/administrator/incubator/MainActivity.java
+++ Incubator-admin/app/src/main/java/com/example/administrator/incubator/MainActivity.java
@@ -1,14 +1,175 @@
-package com.cookandroid.self10_2;
+package com.example.administrator.incubator;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.example.administrator.incubator.R;
+class Person implements Parcelable {
+    String babyName;                    //아이이름
+    String parentName;                  //보호자성함
+    String sex;                         //성별
+    String bloodType;                   //혈액형
+    String ssn;                         //주민번호
+    String state;                       //중증도
+    String temperature;                 //온도
+    String humidity;                    //습도
+    String heartbeat;                   //심박수
+    String sound;                       //소리
+
+    public Person(){
+        this.babyName = "";
+        this.parentName = "";
+        this.sex = "";
+        this.bloodType = "";
+        this.ssn = "";
+        this.state = "";
+        this.temperature = "";
+        this.humidity = "";
+        this.heartbeat = "";
+        this.sound = "";
+
+
+    }
+    public Person(String babyName, String parentName, String sex, String bloodType, String ssn, String state, String temperature, String humidity, String heartbeat, String sound) {
+        this.babyName = babyName;
+        this.parentName = parentName;
+        this.sex = sex;
+        this.bloodType = bloodType;
+        this.ssn = ssn;
+        this.state = state;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.heartbeat = heartbeat;
+        this.sound = sound;
+    }
+
+    public void setBabyName(String babyName) {
+        this.babyName = babyName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setHeartbeat(String heartbeat) {
+        this.heartbeat = heartbeat;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    public String getBabyName() {
+        return babyName;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public String getHeartbeat() {
+        return heartbeat;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.babyName);
+        dest.writeString(this.parentName);
+        dest.writeString(this.sex);
+        dest.writeString(this.bloodType);
+        dest.writeString(this.ssn);
+        dest.writeString(this.state);
+        dest.writeString(this.temperature);
+        dest.writeString(this.humidity);
+        dest.writeString(this.heartbeat);
+        dest.writeString(this.sound);
+
+    }
+    public static final Parcelable.Creator<Person> CREATOR = new Creator<Person>(){
+        @Override
+        public Person createFromParcel(Parcel source) {
+            String babyName = source.readString();                   //아이이름
+            String parentName = source.readString();                 //보호자성함
+            String sex = source.readString();                        //성별
+            String bloodType = source.readString();                  //혈액형
+            String ssn = source.readString();                           //주민번호
+            String state = source.readString();                         //중증도
+            String temperature = source.readString();                 //온도
+            String huminity = source.readString();                    //습도
+            String heartbeat = source.readString();                  //심박수
+            String sound = source.readString();                      //소리
+            return new Person(babyName, parentName, sex, bloodType, ssn, state, temperature, huminity, heartbeat, sound);
+        }
+
+        @Override
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
+}
+
 
 public class MainActivity extends Activity {
 
@@ -19,166 +180,71 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         setTitle("New Concpet of Incubator!!");
 
+        //DB에서 데이터 받아올 변수들
+        String[] babyName = {"성영민", "김건형", "권혁정", "최재원", "김희선", "김태희", "최지우", "문채원", "아이유", "장기하", "이효리", "송혜교", "이병헌", "장동건", "원빈", "서태지"};                    //아이이름
+        String[] parentName = {"아부지", "아부지", "아부지", "아부지", "아부지","아부지","아부지","아부지","아부지","아부지","아부지","아부지","아부지","아부지","아부지","아부지"};                 //보호자성함
+        String[] sex = {"남","여","남","여","남","여","남","여","남","여","남","여","남","여","남","여"};                         //성별
+        String[] bloodType = {"A", "B", "AB", "O", "A", "B", "AB", "O", "A", "B", "AB", "O", "A", "B", "AB", "O"};                  //혈액형
+        String[] ssn = {"900502","880520", "911013", "860611","900502","880520", "911013", "860611","900502","880520", "911013", "860611","900502","880520", "911013", "860611"};                         //주민번호
+        String[] state = {"4", "5", "6","9", "4", "5", "6","9","4", "5", "6","9","4", "5", "6","9"};                       //중증도
+        String[] temperature = {"36.1", "38.2", "37.1", "37.4", "36.1", "38.2", "37.1", "37.4", "36.1", "38.2", "37.1", "37.4", "36.1", "38.2", "37.1", "37.4"};                 //온도
+        String[] humidity = {"70.1", "69.3", "70.7", "70.2","70.1", "69.3", "70.7", "70.2","70.1", "69.3", "70.7", "70.2","70.1", "69.3", "70.7", "70.2"};                    //습도
+        String[] heartbeat = {"62", "58", "60", "64", "62", "58", "60", "64", "62", "58", "60", "64", "62", "58", "60", "64"};                   //심박수
+        String[] sound = {"10.2", "8.8", "9.2", "11.3", "10.2", "8.8", "9.2", "11.3", "10.2", "8.8", "9.2", "11.3", "10.2", "8.8", "9.2", "11.3"};                       //소리
 
-        // 16개의 이미지 버튼 객체배열
-        ImageView image[] = new ImageView[16];
-        // 16개의 이미지버튼 ID 배열
+
+        // 글로벌 액티비티에서 16개의 아이 사진과 아이이름
+        ImageView[] image = new ImageView[16];
+        TextView[] tv = new TextView[16];
+
+        // 16개의 아이 사진 ID 배열
         Integer imageId[] = { R.id.image1, R.id.image2, R.id.image3, R.id.image4, R.id.image5,
-                R.id.image6, R.id.image7, R.id.image8, R.id.image9, R.id.image10, R.id.image11, R.id.image12,
+                R.id.image6, R.id.image7, R.id.image8, R.id.image9, R.id.image10, R.id.image11, R.id.image12, R.id.image13,
                 R.id.image14, R.id.image15, R.id.image16};
 
-        // 16개의 이름 디비로 받을 것인데 음...
-        final String imgName[] = { "김건형", "최재원", "김치남",
-                "성영민", "권혁정", "김건모", "차태현", "이유리", "다니엘 헤니", "김태희", "태연", "송혜교", "문채원", "효린", "비", "세븐" };
+        // 16개의 아이 이름 1D 배열
+        Integer tvID[] = {R.id.tv1,R.id.tv2,R.id.tv3,R.id.tv4,R.id.tv5,R.id.tv6,R.id.tv7,R.id.tv8,R.id.tv9,R.id.tv10,
+                R.id.tv11,R.id.tv12,R.id.tv13,R.id.tv14,R.id.tv15,R.id.tv16};
 
-        for (int i = 0; i < imageId.length; i++) {
-            image[i] = (ImageView) findViewById(imageId[i]);
+        //아이데이터가 들어있는 객체 16개 생성
+        final Person[] baby = new Person[16];
+
+        for(int i=0;i<baby.length;i++){//각각의 객체를 생성한다.
+            baby[i]=new Person();
         }
 
-        //이미지마다 리스너를 달았음. 로컬액티비티를 ResultActivity1~16으로 작성할 것.
-        image[0].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity1.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
+        // 16명 아이 정보 디비로 받을 것임
+        // 아이이륾, 보호자이름, 성별, 혈액형, 주민번호, 중증도, 온도, 습도, 심박수, 소리순으로 데이터 입력
+        for(int i=0;i<baby.length;i++){
+            baby[i].setBabyName(babyName[i]);
+            baby[i].setParentName(parentName[i]);
+            baby[i].setSex(sex[i]);
+            baby[i].setBloodType(bloodType[i]);
+            baby[i].setSsn(ssn[i]);
+            baby[i].setState(state[i]);
+            baby[i].setTemperature(temperature[i]);
+            baby[i].setHumidity(humidity[i]);
+            baby[i].setHeartbeat(heartbeat[i]);
+            baby[i].setSound(sound[i]);
+        }
 
-        image[1].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity2.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
+        //이미지 뷰
+        for (int i = 0; i < imageId.length; i++) {
+            final int index = i;
+            image[index] = (ImageView) findViewById(imageId[index]);
+            tv[index] = (TextView) findViewById(tvID[index]);
 
-        image[2].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity3.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
+            tv[index].setText(baby[index].getBabyName());    //아이 이름 세팅
+            image[index].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {         //이미지마다 리스너 달아서 인텐트를 통해 글로벌 액티비티에 선언된 데이터를 각 로컬액티비티에 뿌려준다.
 
-        image[3].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity4.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
+                    Intent intent = new Intent(getApplicationContext(), BabyActivity1.class);
 
-        image[4].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity5.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[5].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity6.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[6].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity7.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[7].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity8.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[8].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity9.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[9].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity10.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[10].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity11.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[11].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity12.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[12].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity13.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[13].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity14.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[14].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity15.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
-
-        image[15].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity15.class);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
-            }
-        });
+                    intent.putExtra("key", baby[index]);
+                    startActivity(intent);
+                }
+            });
+        }
 
     }
 
